82bc3015a3c31aa8987eaa1278ebce06fdce0e4c
Leonid Burtsev <unoejk@gmail.com>
2024-03-18 00:59:06 +0300

    init project

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import './App.css'
+
+
+// ---- go-go
+
+export default class extends React.Component{
+    render() {return(
+        <h1>Hello there</h1>
+    )}
+}
---
+++ src/index.js
@@ -0,0 +1,17 @@
+
+// ---- import
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import App from './components/App/App'
+
+
+// ---- go-go
+
+const root=ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
+)
